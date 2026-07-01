163f11af0b90539b8baaa6fae80821798fc38ab6
Youssef Mahmoud Hanafy <youssefhanafy325@gmail.com>
2026-07-01 05:39:04 +0300

    Refactor PrismaClient initialization
    Simplified PrismaClient creation by removing datasource configuration.

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -5,13 +5,7 @@ const globalForPrisma = globalThis as unknown as {
 }
 
 function createPrismaClient() {
-  return new PrismaClient({
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL,
-      },
-    },
-  })
+  return new PrismaClient()
 }
 
 export const db = globalForPrisma.prisma ?? createPrismaClient()
